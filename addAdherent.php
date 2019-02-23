3e4e7143cda35a4278f1e1a93c4570c1e587de4a
Nathan <42048771+DevNathan@users.noreply.github.com>
2019-02-23 11:01:37 +0100

    Bcp de nouvelles focntionnalité :
    Upload image : permet de stocker le code base64 d'une image sur le serveur
    Load image : permet d'envoyer le code base64 d'une image stocké sur le serveur
    add adherent : permet d'ajouter un adhérent à la base de donnée
    remove adherent : supprime une adhérent de la base de donnée
    
    Ces 4 fichiers sont mis à la disposition unqiuement des super-admin. Leur mot de passe est donc necessaire

---
+++ addAdherent.php
@@ -0,0 +1,44 @@
+<?php 
+try
+{
+	// On se connecte à MySQL en phase de test
+	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur database: '.$e->getMessage());
+}
+
+
+
+
+$nom = $_POST['Nom'];
+$classe = $_POST['Classe'];
+$dateNaissance = $_POST['DateNaissance'];
+$statut = $_POST['Statut'];
+$imageData = $_POST['ImageData'];
+
+
+//Cette API n'est destinée qu'au membre du bureau. Ainsi sans le nom et le mot de passe elle ne revoie rien du tout
+//Données de l'URL :
+$idAdmin = $_POST['idAdmin'];
+$mdpAdmin = hash('sha256', $_POST['MdpAdmin']);
+
+$checkInfo = $bdd->query('SELECT * FROM ListeAdherents WHERE id="'.$idAdmin.'"');
+
+//On formatte les données récupérés
+$infos = $checkInfo->fetch();
+
+if($infos['Mdp'] != "" && $infos['Mdp'] != "none" && ($infos['Statut'] == "Membre du bureau" || $infos['Statut'] == "Super-admin" || $infos['Statut'] == "Développeur")) { //Si on detecte un mot de passe et qu'il est bien renseigné, et que l'élève est bien accredité 
+	//La connexion est prête. Verification du mdp : 
+	if($infos['Mdp'] == $mdpAdmin) {
+		$bdd->exec('INSERT INTO ListeAdherents(Nom, Statut, DateNaissance, ImageData, PointFidelite, Classe, Mdp) VALUES("'.$nom.'", "'.$statut.'", "'.$dateNaissance.'", "'.$imageData.'", \'0\', "'.$classe.'",\'none\')');
+?>"success"<?php
+	} else {
+		?>"Mot de passe incorrect"<?php
+	}
+} else {
+	?>"Accès au serveur refusé"<?php
+}
+?>
---
+++ uploadImage.php
@@ -0,0 +1,42 @@
+<?php 
+try
+{
+	// On se connecte à MySQL en phase de test
+	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur database: '.$e->getMessage());
+}
+
+
+$id = $_POST['id']; // est totalement unique
+$data = $_POST['data'];
+
+$idAdmin = $_POST['idAdmin'];
+$mdpAdmin = hash('sha256', $_POST['mdpAdmin']);
+
+$checkInfo = $bdd->query('SELECT * FROM ListeAdherents WHERE id="'.$idAdmin.'"');
+
+//On formatte les données récupérés
+$infos = $checkInfo->fetch();
+
+if($infos['Mdp'] != "" && $infos['Mdp'] != "none" && ($infos['Statut'] == "Membre du bureau" || $infos['Statut'] == "Super-admin" || $infos['Statut'] == "Développeur")) { //Si on detecte un mot de passe et qu'il est bien renseigné, et que l'élève est bien accredité 
+	//La connexion est prête. Verification du mdp : 
+	if($infos['Mdp'] == $mdpAdmin) {
+		$bdd->exec('UPDATE ListeAdherents SET ImageData = "'.$data.'" WHERE id = "'.$id.'"');
+		//Ne peut être executé que par un admin/super-admin/developpeur
+	} else {
+		?>"Accès au serveur refusé"<?php
+	}
+} else {
+	?>"Accès au serveur refusé"<?php
+}
+
+
+?>
+
+
+
+?>
